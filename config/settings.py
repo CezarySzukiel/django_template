e07062e7c1a0b0cd73c6b127c8f601eeacfdcd24
Cezary <cezary.szukiel@gmail.com>
2023-08-30 14:55:36 +0200

    add secret key using environment variables

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o9+%pvz3=8ia9o%t9*o%=!@$tu89lmjvd8t(5_6kg_ck*p9_7c'
+SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
