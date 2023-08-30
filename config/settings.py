7cd64c24ecfbd0dbc3fc736f3175957b2af2b977
Cezary <cezary.szukiel@gmail.com>
2023-08-30 14:05:04 +0200

    add custom migration for superuser and configure env variables using django-environ package

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env'))
+# os.path.join - łączy ścieżki za pomocą separatora ścieżek systemu operacyjnego.
+# environ nie zczytuje pliku i nie tworzy lokalnie zmiennych, tylko wczytuje je do sesji
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
